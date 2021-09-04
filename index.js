c3a6bbbafa5e9acb813e44be44df52d0cba84858
Keshee Miles <keshee.miles18@gmail.com>
2021-09-03 23:05:02 -0500

    homework week two

---
+++ index.js
@@ -0,0 +1,12 @@
+const PizzaPlace = "SaucyDough"
+let numberOfToppings = 07;
+
+console.log(typeof PizzaPlace, PizzaPlace);
+console.log(typeof numberOfToppings, numberOfToppings);
+
+
+if (numberOfToppings < 10) {
+    console.log("Quality,not quanity");
+  } else {
+    console.log("A whole lot of pizza.");
+  }
