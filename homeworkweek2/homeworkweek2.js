77e34b4522a3bc1a320d96e548347dab173dd6b3
Keshee Miles <keshee.miles18@gmail.com>
2021-09-05 12:19:54 -0500

    pushing week 2 hw to github

---
+++ homeworkweek2/homeworkweek2.js
@@ -0,0 +1,19 @@
+const PizzaPlace = "SaucyDough"
+let numberOfToppings = 7;
+
+console.log (`Hello, welcome to ${PizzaPlace}  where we serve ${numberOfToppings}  different  delicious toppings!! `);
+
+console.log(typeof PizzaPlace, PizzaPlace);
+console.log(typeof numberOfToppings, numberOfToppings);
+
+
+
+
+if (numberOfToppings < 10) {
+    console.log("Quality,not quanity");
+  } else {
+    console.log("A whole lot of pizza.");
+  }
+
+
+
