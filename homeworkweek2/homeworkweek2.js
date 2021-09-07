1b1362bfb181b155899015fce929cc791151bf97
Keshee Miles <keshee.miles18@gmail.com>
2021-09-06 19:27:50 -0500

    updated file
    updated file readme

--- homeworkweek2/homeworkweek2.js
+++ homeworkweek2/homeworkweek2.js
@@ -1,19 +1,15 @@
-const PizzaPlace = "SaucyDough"
+const PizzaPlace = "SaucyDough";
 let numberOfToppings = 7;
 
-console.log (`Hello, welcome to ${PizzaPlace}  where we serve ${numberOfToppings}  different  delicious toppings!! `);
+console.log(
+  `Hello, welcome to ${PizzaPlace}  where we serve ${numberOfToppings}  different  delicious toppings!! `
+);
 
 console.log(typeof PizzaPlace, PizzaPlace);
 console.log(typeof numberOfToppings, numberOfToppings);
 
-
-
-
 if (numberOfToppings < 10) {
-    console.log("Quality,not quanity");
-  } else {
-    console.log("A whole lot of pizza.");
-  }
-
-
-
+  console.log("Quality,not quantity");
+} else {
+  console.log("A whole lot of pizza.");
+}
