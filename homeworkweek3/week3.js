fe19308fb4c396acad133b58a78c4911ffda1b8e
Keshee Miles <keshee.miles18@gmail.com>
2021-09-14 00:47:40 -0500

    pushhomwork3

---
+++ homeworkweek3/week3.js
@@ -0,0 +1,37 @@
+let pizzaToppings = ["Sausage", "Onions", "Bacon", "Pineapple"];
+function greetCustomer() {
+  let greeting = "Welcome to SaucyDough, our toppings are:";
+  for (let topping of pizzaToppings) {
+    greeting += `${topping}, `;
+  }
+  console.log(greeting);
+}
+greetCustomer();
+
+function getPizzaOrder(size, crust, ...toppings) {
+  console.log(`one ${size} ${crust} crust with  ${toppings} coming up`);
+}
+getPizzaOrder("large", "thin", "sausage", "bacon");
+
+function preparePizza(...myArray) {
+  const myPizzaObject = {
+    Size: myArray[0],
+    Crust: myArray[1],
+    Toppings: []
+  };
+  console.log(myArray.length);
+  // const myL=myArray.length;
+  let x = 2;
+  for (x = 2; x < myArray.length; x++) {
+    myPizzaObject.Toppings[x] = myArray[x];
+    console.log(myPizzaObject.Toppings[x], x);
+  }
+}
+preparePizza(pizzaToppings);
+//  console.log(get)
+//  preparePizza();
+
+// function preparePizza(size, crust, ...toppings);
+
+// function servePizza();{
+//   size: "large";
